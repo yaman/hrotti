fb073d0b853928358d15e4ad62a6562ed46b5eb2
abdulkadiryaman <abdulkadiryaman@gmail.com>
2014-05-17 18:48:29 +0300

    travis integration

--- main.go
+++ main.go
@@ -1,47 +1,22 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
+import (    "os"
 	"os/signal"
 	"syscall"
 
-	. "github.com/alsm/hrotti/broker"
+	. "github.com/abdulkadiryaman/hrotti/broker"
+	"fmt"
 )
 
-func createConfig() BrokerConfig {
-	configFile := flag.String("conf", "", "A configuration file")
-
-	flag.Parse()
-
-	var config BrokerConfig
-	config.Listeners = make(map[string]*ListenerConfig)
-
-	if *configFile == "" {
-		listener := NewListenerConfig(os.Getenv("HROTTI_URL"))
-		if listener.URL.Host == "" {
-			listener = NewListenerConfig("tcp://0.0.0.0:1883")
-		}
-		config.Listeners["envconfig"] = listener
-	} else {
-		err := ParseConfig(*configFile, &config)
-		if err != nil {
-			os.Stderr.WriteString(fmt.Sprintf("%s\n", err.Error()))
-		}
-	}
-	config.SetLogTargets()
-	return config
-}
-
 func main() {
-	config := createConfig()
+	listener := NewListenerConfig("tcp://0.0.0.0:1883")
 
-	h := NewHrotti(config.MaxQueueDepth)
+	h := NewHrotti(100)
 
-	for name, listener := range config.Listeners {
-		h.AddListener(name, listener)
-	}
+	fmt.Println("starting app")
+
+	h.AddListener("self", listener)
+
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	<-c
